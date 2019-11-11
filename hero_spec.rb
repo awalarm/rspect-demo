9dedf77951386d72f428687a36fc7c22740a707a
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-11 13:33:55 +0400

    add power_down and hero_info test

--- hero_spec.rb
+++ hero_spec.rb
@@ -14,6 +14,19 @@ describe Hero do
     expect(hero.power_up).to eq 110
   end
 
+  it "can power down" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_down).to eq 90
+  end
+
+  it "displays full hero info" do
+    hero = Hero.new 'mike'
+
+    expect(hero.hero_info).to eq 'Mike ваше здоровье 100'
+
+  end
+
 end
 
 
